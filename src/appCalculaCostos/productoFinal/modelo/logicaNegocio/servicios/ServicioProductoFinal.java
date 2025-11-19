77525690767b9dec4ae2e37314a3ac7e886afd7b
Acatio <acaciopedrozaje@gmail.com>
2025-11-18 23:10:55 -0600

    se hizo ver resumen de Mano de obra

--- src/appCalculaCostos/productoFinal/modelo/logicaNegocio/servicios/ServicioProductoFinal.java
+++ src/appCalculaCostos/productoFinal/modelo/logicaNegocio/servicios/ServicioProductoFinal.java
@@ -209,8 +209,10 @@ public class ServicioProductoFinal
             List<CostoDto> costosDelProdutco = new ArrayList<>();
             var costoMp = repo.obtenerCostoMateriaPrima(idProducto);
             var costoF = repo.obtenerCostoFijoAsignado(idProducto);
-            costosDelProdutco.add(new CostoDto("Costo de materia Prima", Redondeo.redondear(costoMp, 2)));
+            var costoMo=repo.obtenerCostosMoAsignados(idProducto);
+            costosDelProdutco.add(new CostoDto("Costo de Materia Prima", Redondeo.redondear(costoMp, 2)));
             costosDelProdutco.add(new CostoDto("Costos Fijos", Redondeo.redondear(costoF, 2)));
+            costosDelProdutco.add(new CostoDto("Costos Mano de Obra", Redondeo.redondear(costoMo, 2)));
             return costosDelProdutco;
         } catch (PersistenciaException ex)
         {
